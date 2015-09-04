f97053e1fa09aecc0a03c50284d5247c270a838a
KimeT <kimmo.tahvanainen@gmail.com>
2015-09-04 16:28:47 +0300

    Initial commit

---
+++ DNA-TV-EPG-Custom-Search.user.js
@@ -0,0 +1,39 @@
+﻿// ==UserScript==
+// @name         DNA TV EPG Custom Search
+// @namespace    tv.dna.fi
+// @description  Adds custom DNA TV EPG search based on querystring
+// @downloadURL  https://gist.github.com/KimeT/839a31086d57c8726d91/raw/DNA-TV-EPG-Custom-Search.user.js
+// @icon         https://tv.dna.fi/images/matkatv/favicon.ico
+// @match        https://tv.dna.fi/webui/epg*
+// @version      0.4
+// @author       Kimmo Tahvanainen
+// @homepage     https://gist.github.com/KimeT
+// @grant        none
+// ==/UserScript==
+
+/* Custom styles to be added on page */
+var styles =  '';
+
+/* Custom (jQuery) script to be added on page, needs the page to have jQuery already */
+var main = function () {
+	// Custom search action after DOM is ready
+	$(function() {
+		var searchTerm = decodeURI(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURI('customsearch').replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1")).replace(/%26/g, '&').replace(/\+/g,' ')
+		if (searchTerm != null && searchTerm.length) {
+			$('#keyword').val(searchTerm);
+			$('#search-btn').click();
+		}
+	});
+};
+
+/* Inject custom styles */
+/*var style = document.createElement('style');
+style.type = "text/css";
+style.textContent = styles.toString();
+document.head.appendChild(style);*/
+
+/* Inject main script */
+var script = document.createElement('script');
+script.type = "text/javascript";
+script.textContent = '(' + main.toString() + ')();';
+document.body.appendChild(script);
---
+++ DNA-TV-Links-to-Telkku-Programs.user.js
@@ -0,0 +1,38 @@
+﻿// ==UserScript==
+// @name         DNA TV Links to Telkku Programs
+// @namespace    telkku.com
+// @description  Adds custom DNA TV search links to Telkku program pages
+// @downloadURL  https://gist.github.com/KimeT/726de9e3370adc480816/raw/DNA-TV-Links-to-Telkku-Programs.user.js
+// @icon         http://www.telkku.com/favicon.ico
+// @match        http://classic.telkku.com/program/show/*
+// @version      0.1
+// @author       KimeT
+// @homepage     https://gist.github.com/KimeT
+// @grant        none
+// ==/UserScript==
+
+/* Custom styles to be added on page */
+var styles =  '';
+
+/* Custom (jQuery) script to be added on page, needs the page to have jQuery already */
+var main = function () {
+	// Create search link after DOM is ready
+	$(function() {
+		var programName = $('#programName').text().trim().replace(/ /g, '+').replace(/&/g, '%26');
+		var searchUrl = 'https://tv.dna.fi/webui/epg' + (programName.length ? '?customsearch=' + programName : '');
+		var $elem = '<li><a href="' + searchUrl + '" target="_blank" title="DNA TV" class="btn">DNA TV</a></li>';
+		$('nav.oti-outbound-nav:first ul').append($elem);
+	});
+};
+
+/* Inject custom styles */
+/*var style = document.createElement('style');
+style.type = "text/css";
+style.textContent = styles.toString();
+document.head.appendChild(style);*/
+
+/* Inject main script */
+var script = document.createElement('script');
+script.type = "text/javascript";
+script.textContent = '(' + main.toString() + ')();';
+document.body.appendChild(script);
